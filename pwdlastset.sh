520b11bdca03697dad13b864a7e21cddfe73c28a
galiral <galiral@gmail.com>
2014-04-14 12:00:10 +0200

    Update pwdlastset.sh
    Added UserAccountControl flags to skip users and a debug mode.

--- pwdlastset.sh
+++ pwdlastset.sh
@@ -7,24 +7,51 @@ from_mail_address="***********"
 bcc_mail_address="**************"
 zimbra_domain_tocheck="*******"
 reset_password_page="**********"
-# cerca solo gli utenti in Users, che siano di classe user e che abbiano l'attributo email
+DEBUG="FALSE"
+# search only users in Users, that have the user class and with a mail attribute
 
 while read utente 
 	do
 	base_search="dc=contoso,dc=com sAMAccountName=$utente mail"
 	timestamp=`date +%s`
-
-	### ottieni l'attributo pwdlastset da AD
+	useraccountcontrol=`/opt/zimbra/bin/ldapsearch -LLL -D $bind_user -w $bind_pass -h $ldap_server -b $base_search userAccountControl | grep userAccountControl | cut -c 21-`
+	# echo "$useraccountcontrol"
+	if [[ $useraccountcontrol -eq 544 ]];
+	then
+		echo "$utente -> Enabled, Password Not Required"
+	else
+		if [[ $useraccountcontrol -eq 546 ]];
+		then
+			echo "$utente -> Disabled, Password Not Required"
+		else
+			if [[ $useraccountcontrol -eq 66048 ]];
+			then
+				echo "$utente -> Enabled, Password Doesn't Expire"
+					else
+				if [[ $useraccountcontrol -eq 66050 ]];
+				then
+					echo "$utente -> Disabled, Password Doesn't Expire"
+				else
+					if [[ $useraccountcontrol -eq 66080 ]];
+					then
+						echo "$utente -> Enabled, Password Doesn't Expire & Not Required"
+					else
+						if [[ $useraccountcontrol -eq 66082 ]];
+						then
+							echo "$utente -> Disabled, Password Doesn't Expire & Not Required"
+						else
+					
+	### get the pwdlastset attribute from AD
 
         ad_pwdlastset=`/opt/zimbra/bin/ldapsearch -LLL -D $bind_user -w $bind_pass -h $ldap_server -b $base_search pwdLastSet | grep -i pwdLastSet: | cut -c 13-`
 
         if [[ -z "$ad_pwdlastset" ]]; 
 	then
-		echo "no pwdlastset"
+		echo "$utente -> empty pwdLastSet value."
 	else
 		if [[ $ad_pwdlastset -lt 0 ]];
 		then
-			echo "valore pwdLastSet non valido, salto utente"
+			echo "$utente -> pwdLastSet value not valid, skipping."
 		else
 		
 			### converti ad_pwdlastset in unix time (epoch) -> epoch_pwdlastset
@@ -33,17 +60,27 @@ while read utente
 			days_difference=$(($epoch_days/86400))  #converte la differenza in giorni
 			if [[ $days_difference -gt $max_pwd_expiration ]];
 			then
-				echo "la password ha superato i $max_pwd_expiration giorni, potrebbe non scadere o non ha cambiato password nel tempo limite"
+				echo "$utente -> the password superseeded the $max_pwd_expiration days."
 			else			
 			days_remaining=$(($max_pwd_expiration-$days_difference))
 				if [[ $days_difference -gt 75 ]];
 				then
+					if [[ $DEBUG == "TRUE" ]];
+					then
+						echo "$utente -> debug mode, not sending email"
+					else
 					mail_address=`/opt/zimbra/bin/ldapsearch -LLL -D $bind_user -w $bind_pass -h $ldap_server -b $base_search mail | grep -i mail: | cut -c 7-`
 					/usr/local/bin/sendEmail -f $from_mail_address -t $mail_address -bcc $bcc_mail_address -u "Avviso scadenza password domain" -m "la password di $mail_address sta per scadere entro $days_remaining giorni, si consiglia di cambiarla andando su $reset_password_page , usando come nome utente $utente ; la password deve avere minimo di 8 caratteri, con una lettera maiuscola ed almeno un numero." -o tls=no
 				else
-					echo "non sta scadendo"
+					echo "$utente -> there are $days_remaining days until expiration."
 				fi
 			fi		
 		fi
 	fi
+fi 
+fi 
+fi 
+fi 
+fi 
+fi
 done < <(/opt/zimbra/bin/zmprov -l gaa $zimbra_domain_tocheck | cut -d @ -f1)
